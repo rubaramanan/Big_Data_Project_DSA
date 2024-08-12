6c9c85c823e92ebe546eae653ef314f7d43a6953
Ramanan <rubaramanan1997@gmail.com>
2024-08-12 09:46:29 -0400

    documents

--- airflow_pipeline/dags/scripts/postgresqlHandler.py
+++ airflow_pipeline/dags/scripts/postgresqlHandler.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 
 
 def save_data(data):
-    engine = create_engine('postgresql://postgres:aymane2002@localhost:5432/Big-Data-Project')
+    engine = create_engine('postgresql://dsa:dsa@192.168.2.79:5432/dsafinal')
 
-    data.to_sql('Phone', engine, if_exists='replace', index=False)
+    data.to_sql('sales', engine, if_exists='replace', index=False)
 
     print("data stored in postgresql")
